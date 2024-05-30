d312ece77cc0167b0f80d8b0eaba0ebd3e009fc1
rafacandido7 <rafacandido0708@gmail.com>
2024-05-30 19:37:26 -0300

    feat: nick & modt commands

--- servidor.py
+++ servidor.py
@@ -6,48 +6,44 @@ from collections import deque
 from _thread import *
 from utils import *
 
-MOTD = '''SOCKER IRC SERVER v0.1'''
+MOTD = '''SOCKER IRC SERVER v0.1 \n by rafael'''
 
 class Cliente:
 
     def __init__(self, conn, addr):
         self.conn = conn
         self.addr = addr
-        self.buffer = ""
-        self.nome = None
+        self.nick = None
 
 
     def receber_dados(self):
         try:
             data = decode(self.conn)
-            print(data)
-            self.buffer += data
+            print_receive(data)
 
-            while '\r\n' in self.buffer:
-                line, self.buffer = self.buffer.split('\r\n', 1)
-                self.processar_comando(line)
-
-            return True
+            return data
         except error:
             print(error)
             return False
 
-    def processar_comando(self, line):
-        print_receive(line)
+    def enviar_mensagem(self, msg):
+        try:
+            print_send(msg)
+            self.conn.sendall(msg.encode())
+        except error:
+            print(error)
+            self.conn.close()
 
-        # if line.startswith("NICK"):
-        #     self.nome = line.split(':')[1]
-        #     self.enviar_dados(f":{self.nome} NICK recebido\r\n")
-        # elif line.startswith("QUIT"):
-        #     self.enviar_dados(f"Desconectando: {self.nome}\r\n")
-        #     self.conn.close()
-
-    def enviar_dados(self, msg):
+    def enviar_mensagem_geral(self, msg):
         try:
             self.conn.sendall(msg.encode())
         except error:
             print(error)
             self.conn.close()
+
+    def enviar_pong(self, payload):
+        self.enviar_mensagem(f"PONG :{payload}\r\n")
+
 
 class Servidor:
     def __init__(self,host='localhost',  port=6667, debug=False):
@@ -58,25 +54,63 @@ class Servidor:
         self.conns = deque()
         self.port = port
         self.debug = debug
-        self.host = host
+        self.host = 'localhost'
+        self.clientes = []
+        self.nicks = {}
 
 
     def run(self, conn, addr):
         cliente = Cliente(conn, addr)
-        # na primeira conexão, eu preciso esperar ele enviar as 2 mensagens? dai depois posso enviar o motd?
-
-        self.conns.append(cliente)
 
         while True:
-            res = cliente.receber_dados()
-            conn.send(MOTD.encode())
-            print_send(MOTD)
-            if not res:
-                break
+            dados = cliente.receber_dados()
+            while '\r\n' in dados:
+                line, dados = dados.split('\r\n', 1)
+                if line:
+                    self.processar_comando(cliente, line)
 
-        self.conns.remove(cliente)
-        pass
+    def processar_comando(self, cliente, dados):
+        if dados.startswith('NICK'):
+            self.processar_nick(cliente, dados.split()[1])
+        elif dados.startswith('USER'):
+            self.processar_user(cliente, dados.split()[1], dados.split(':')[1])
 
+    def processar_nick(self, cliente, nick):
+        if not validate_nick(nick):
+            cliente.enviar_mensagem(f"432 * {nick} :Erroneous Nickname\r\n")
+        elif nick.lower() in self.nicks:
+            cliente.enviar_mensagem(f"433 * {nick} :Nickname is already in use\r\n")
+        else:
+            if cliente.nick:
+                del self.nicks[cliente.nick.lower()]
+                mensagem = f":{cliente.nick} NICK {nick}\r\n"
+                self.broadcast(mensagem)
+                cliente.nick = nick
+            else:
+                cliente.enviar_mensagem(f"001 {nick} :Welcome to the Internet Relay Network {nick}\r\n")
+                cliente.nick = nick
+                self.enviar_motd(cliente)
+
+            self.nicks[nick.lower()] = cliente
+
+    def enviar_motd(self, cliente):
+        cliente.enviar_mensagem(f"375 {cliente.nick} : {self.host} - Message of the Day -\r\n")
+        for line in MOTD.split('\n'):
+            cliente.enviar_mensagem(f"372 {cliente.nick} :- {line}\r\n")
+
+        cliente.enviar_mensagem(f"376 {cliente.nick} :End of /MOTD command.\r\n")
+
+    def processar_user(self, cliente, username, realname):
+        cliente.username = username
+        cliente.realname = realname
+
+    def processar_ping(self, cliente, payload):
+        cliente.enviar_pong(payload)
+
+    def broadcast(self, mensagem):
+        print_send(mensagem)
+        for cliente in self.clientes:
+            cliente.enviar_mensagem_geral(mensagem)
 
     def listen(self):
         '''
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import re
+
 def print_receive(str):
     print(f"\033[96m>>> {str}\033[0m")
 
@@ -8,4 +10,16 @@ def print_error(str):
     print(f"\033[91m{str}\033[0m")
 
 def decode(conn):
-    return conn.recv(1024).decode()
+    return conn.recv(1024).decode().strip()
+
+def validate_nick(nick):
+    if len(nick) > 9:
+        return False
+
+    if not nick[0].isalpha():
+        return False
+
+    if not re.match("^[A-Za-z0-9_]*$", nick):
+        return False
+
+    return True
